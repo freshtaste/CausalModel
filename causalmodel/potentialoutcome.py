8ac35b27e9f0dd339bb2f3731ddfe1035b0eaf8f
jjzfsb <liujizhou123@hotmail.com>
2020-10-31 22:00:43 -0700

    added mathcing

--- causalmodel/potentialoutcome.py
+++ causalmodel/potentialoutcome.py
@@ -47,7 +47,9 @@ class PotentialOutcome(Model):
         else:
             G = w1 * self.data.Y - w0 * self.data.Y 
         
-        return self._get_results(G)
+        ate = np.mean(G)
+        se = np.sqrt(np.var(G) / (len(G)-1))
+        return self._get_results(ate, se)
     
     
     def est_via_aipw(self, OutcomeModel, PropensityModel, treated_pred=None, 
@@ -78,7 +80,62 @@ class PotentialOutcome(Model):
              + self.data.Z * (self.data.Y - self.treated_pred)/ self.propensity 
              - (1 - self.data.Z) * (self.data.Y - self.control_pred)/ (1-self.propensity))
         
-        return self._get_results(G)
+        ate = np.mean(G)
+        se = np.sqrt(np.var(G) / (len(G)-1))
+        return self._get_results(ate, se)
+    
+    
+    def est_via_matching(self, num_matches, num_matches_for_var, bias_adj=False):
+        
+        Xt, Yt, Xc, Yc = self.data.Xt, self.data.Yt, self.data.Xc, self.data.Yc
+        nt, nc, n = self.data.nt, self.data.nc, self.data.n
+        M, J = num_matches, num_matches_for_var
+
+        # standardizing the covariates and construct the matrix of all differences
+        sd_Xt, sd_Xc = np.sqrt(np.var(Xt, axis=0)), np.sqrt(np.var(Xc, axis=0))
+        Xt_scaled, Xc_scaled = Xt/sd_Xt, Xc/sd_Xc
+        X_diff = Xt_scaled[:,np.newaxis] - Xc_scaled
+
+        # compute Nt by Nc matrix of distances and find top m indices
+        X_dist = np.sum(X_diff**2, axis=2)
+        match_for_t, match_for_c = np.argpartition(X_dist, M)[:,:M], \
+            np.argpartition(X_dist.T, M)[:,:M]
+        Yhat_c, Yhat_t = np.mean(Yt[match_for_c],axis=1), np.mean(Yc[match_for_t],axis=1)
+        ITT_t, ITT_c = Yt - Yhat_t, Yhat_c - Yc
+        Yhat1, Yhat0 = np.append(Yt, Yhat_c), np.append(Yhat_t, Yc)
+
+        if bias_adj:
+            pass
+        
+        atc, att = ITT_c.mean(), ITT_t.mean()
+        ate = (nc/n)*atc + (nt/n)*att
+        
+        # estimate variance
+        Km = np.zeros(n)
+        for row in match_for_c:
+            Km[row] += 1
+        for row in match_for_t:
+            Km[row+nt] += 1
+        
+        # 1. match treated to treated, control to control
+        X_diff_t = Xt_scaled[:,np.newaxis] - Xt_scaled
+        X_diff_c = Xc_scaled[:,np.newaxis] - Xc_scaled
+        X_dist_t = np.sum(X_diff_t**2, axis=2)
+        X_dist_c = np.sum(X_diff_c**2, axis=2)
+        match_tt, match_cc = np.argpartition(X_dist_t, J)[:,:J], \
+            np.argpartition(X_dist_c, J)[:,:J]
+        Yhat_cc, Yhat_tt = np.mean(Yc[match_cc],axis=1), np.mean(Yt[match_tt],axis=1)
+        Y, Y_close = np.append(Yt, Yc), np.append(Yhat_tt, Yhat_cc)
+        # 2. estimate conditional variance
+        sigmaXW = J/(J+1)*((Y - Y_close)**2)
+        # 3. compute variance
+        V1 = (Yhat1 - Yhat0 - ate)**2
+        V2 = ((Km/M)**2 + (2*M-1)/M * Km/M)*sigmaXW
+        V = np.mean(V1 + V2)
+        
+        se = np.sqrt(V/n)
+        return self._get_results(ate, se)
+        
     
     
     def _fix_propensity(self):
@@ -91,9 +148,7 @@ class PotentialOutcome(Model):
                               .format(num_bad_prop))
     
     
-    def _get_results(self, G):
-        ate = np.mean(G)
-        se = np.sqrt(np.var(G) / (len(G)-1))
+    def _get_results(self, ate, se):
         self.result = Result(average_treatment_effect=ate,
                              standard_error=se,
                              z=ate/se,
@@ -114,6 +169,8 @@ class POdata(object):
             self.n = self.get_n()
             self.idx_t = self.Z == 1
             self.idx_c = self.Z == 0
+            self.nc = np.sum(self.idx_c)
+            self.nt = np.sum(self.idx_t)
             self.Yc = self.get_Yc()
             self.Yt = self.get_Yt()
             self.Xc = self.get_Xc()
