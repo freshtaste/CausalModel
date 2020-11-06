ac37cec2526423db710890ca3be27e019e20c99c
jjzfsb <liujizhou123@hotmail.com>
2020-11-05 20:26:57 -0800

    fix runtime

--- causalmodel/potentialoutcome.py
+++ causalmodel/potentialoutcome.py
@@ -83,30 +83,31 @@ class PotentialOutcome(Model):
         ate = np.mean(G)
         se = np.sqrt(np.var(G) / (len(G)-1))
         return self._get_results(ate, se)
-    
+        
     
     def est_via_matching(self, num_matches, num_matches_for_var, bias_adj=False):
         
         Xt, Yt, Xc, Yc = self.data.Xt, self.data.Yt, self.data.Xc, self.data.Yc
         nt, nc, n = self.data.nt, self.data.nc, self.data.n
         M, J = num_matches, num_matches_for_var
+        if J is None:
+            J = M
 
-        # standardizing the covariates and construct the matrix of all differences
+        # standardizing the covariate matrices and match
         sd_Xt, sd_Xc = np.sqrt(np.var(Xt, axis=0)), np.sqrt(np.var(Xc, axis=0))
         Xt_scaled, Xc_scaled = Xt/sd_Xt, Xc/sd_Xc
-        X_diff = Xt_scaled[:,np.newaxis] - Xc_scaled
-
-        # compute Nt by Nc matrix of distances and find top m indices
-        X_dist = np.sum(X_diff**2, axis=2)
-        match_for_t, match_for_c = np.argpartition(X_dist, M)[:,:M], \
-            np.argpartition(X_dist.T, M)[:,:M]
+        match_for_t, match_for_c = self.mat_match_mat(Xt_scaled, Xc_scaled, M), \
+            self.mat_match_mat(Xc_scaled, Xt_scaled, M)
+        
+        # compute ate
         Yhat_c, Yhat_t = np.mean(Yt[match_for_c],axis=1), np.mean(Yc[match_for_t],axis=1)
         ITT_t, ITT_c = Yt - Yhat_t, Yhat_c - Yc
         Yhat1, Yhat0 = np.append(Yt, Yhat_c), np.append(Yhat_t, Yc)
         
         atc, att = ITT_c.mean(), ITT_t.mean()
         ate = (nc/n)*atc + (nt/n)*att
-
+        
+        # adjust for bias
         if bias_adj:
             mu0 = OLS().fit(Xc, Yc)
             mu1 = OLS().fit(Xt, Yt)
@@ -116,7 +117,7 @@ class PotentialOutcome(Model):
             match_for_1c = np.mean(mu1_t[match_for_c],axis=1)
             BM = np.sum(mu0_t - match_for_0t)/n - np.sum(mu1_c - match_for_1c)/n
             ate -= BM
-        
+            
         # estimate variance
         Km = np.zeros(n)
         for row in match_for_c:
@@ -125,16 +126,12 @@ class PotentialOutcome(Model):
             Km[row+nt] += 1
         
         # 1. match treated to treated, control to control
-        X_diff_t = Xt_scaled[:,np.newaxis] - Xt_scaled
-        X_diff_c = Xc_scaled[:,np.newaxis] - Xc_scaled
-        X_dist_t = np.sum(X_diff_t**2, axis=2)
-        X_dist_c = np.sum(X_diff_c**2, axis=2)
-        match_tt, match_cc = np.argpartition(X_dist_t, J)[:,:J], \
-            np.argpartition(X_dist_c, J)[:,:J]
+        match_tt, match_cc =  self.mat_match_mat(Xt_scaled, Xt_scaled, J+1), \
+            self.mat_match_mat(Xc_scaled, Xc_scaled, J+1)
         Yhat_cc, Yhat_tt = np.mean(Yc[match_cc],axis=1), np.mean(Yt[match_tt],axis=1)
         Y, Y_close = np.append(Yt, Yc), np.append(Yhat_tt, Yhat_cc)
         # 2. estimate conditional variance
-        sigmaXW = J/(J+1)*((Y - Y_close)**2)
+        sigmaXW = (J+1)/J*((Y - Y_close)**2)
         # 3. compute variance
         V1 = (Yhat1 - Yhat0 - ate)**2
         V2 = ((Km/M)**2 + (2*M-1)/M * Km/M)*sigmaXW
@@ -142,7 +139,6 @@ class PotentialOutcome(Model):
         
         se = np.sqrt(V/n)
         return self._get_results(ate, se)
-        
     
     
     def _fix_propensity(self):
@@ -162,7 +158,20 @@ class PotentialOutcome(Model):
                              p_value=((1 - norm.cdf(ate/se))*2),
                              confidence_interval=(ate - 1.96*se, ate+1.96*se))
         return self.result
-
+    
+    
+    def mat_match_mat(self, X, Y, M):
+        return np.array([self.arr_match_mat(Xi, Y, M) for Xi in X])
+         
+    
+    def dist_arr_mat(self, Xi, X):
+        diff = X - Xi
+        return np.sum(diff**2,axis=1)
+        
+    
+    def arr_match_mat(self, Xi, X, M):
+        dist = self.dist_arr_mat(Xi, X)
+        return np.argpartition(dist, M)[:M]
     
     
 class POdata(object):
