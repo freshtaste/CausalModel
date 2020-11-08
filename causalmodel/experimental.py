7a0f36f3b879989c4868f36a2f9cb435c33791bc
freshtaste <liujizhou123@hotmail.com>
2020-11-07 21:30:08 -0800

    add ancova and strata

--- causalmodel/experimental.py
+++ causalmodel/experimental.py
@@ -1,5 +1,6 @@
 import numpy as np
 from potentialoutcome import PotentialOutcome
+import statsmodels.api as sm 
 
 
 class Experimental(PotentialOutcome):
@@ -16,18 +17,36 @@ class Experimental(PotentialOutcome):
     def est_via_dm(self):
         self.cal_stats = lambda Z: np.mean(self.data.Y[Z==1]) - np.mean(self.data.Y[Z==0])
         self.stats = self.cal_stats(self.data.Z)
-        v1 = np.var(self.data.Yt)
-        v2 = np.var(self.data.Yc)
-        se = np.sqrt(v1/self.data.nt + v2/self.data.nc)
-        return self._get_results(self.stats, se)
+        ate, se = self.cal_dm(self.data.Z, self.data.Y)
+        return self._get_results(ate, se)
         
     
-    def est_via_strata(self):
-        pass
-    
+    def est_via_strata(self, strata):
+        if len(strata) != self.data.n:
+            raise RuntimeError("input doesn't have the same length as the data.")
+        if type(strata) != np.ndarray:
+            raise TypeError("input must be numpy array.")
+        ate_list = list()
+        se_list = list()
+        for l in set(strata):
+            w = np.mean(strata==l)
+            ate_s, se_s = self.cal_dm(self.data.Z[strata==l], self.data.Y[strata==l])
+            ate_list.append(ate_s*w)
+            se_list.append((se_s**2)*(w**2))
+        ate = np.sum(ate_list)
+        se = np.sqrt(np.sum(se_list))
+        return self._get_results(ate, se)
+
     
     def est_via_ancova(self):
-        pass
+        Z = self.data.Z.reshape(-1,1)
+        regressor = np.concatenate((np.ones((self.data.n,1)), Z, 
+                              self.data.X, self.data.X * Z), axis=1)
+        ols = sm.OLS(self.data.Y, regressor).fit()
+        ate = ols.params[1]
+        se = ols.HC0_se[1]
+        return self._get_results(ate, se)
+        
     
     
     def test_via_fisher(self, n=1000):
@@ -37,6 +56,14 @@ class Experimental(PotentialOutcome):
             T_s[i] = self.cal_stats(Z_s)
         pval = min(np.mean(T_s > self.stats), np.mean(T_s < self.stats))
         return pval
+    
+    
+    def cal_dm(self, Z, Y):
+        ate = np.mean(Y[Z==1]) - np.mean(Y[Z==0])
+        v1 = np.var(Y[Z==1])
+        v2 = np.var(Y[Z==0])
+        se = np.sqrt(v1/np.sum(Z==1)+ v2/np.sum(Z==0))
+        return ate, se
             
     
     def load_design(self, design):
