b98d281ec6b34425fe774b5509fc834a8fd58ac1
freshtaste <liujizhou123@hotmail.com>
2020-12-09 21:31:02 -0800

    add notations

--- causalmodel/experimental.py
+++ causalmodel/experimental.py
@@ -5,8 +5,11 @@ import statsmodels.api as sm
 
 
 class Experimental(PotentialOutcome):
+    """Main class for experimental data."""
     
     def __init__(self, Y, Z, X=None, design=CRD()):
+        """For experimental data, covariates and an instance of design class
+        are optional."""
         if X is None:
             X = np.ones(Y.shape)
         super(self.__class__, self).__init__(Y,Z,X)
@@ -17,10 +20,12 @@ class Experimental(PotentialOutcome):
         
         
     def estimate(self):
+        """Default estimation is a difference-in-mean estimator."""
         return self.est_via_dm()
     
     
     def est_via_dm(self):
+        """Difference-in-mean estimator"""
         self.cal_stats = lambda Z: np.mean(self.data.Y[Z==1]) - np.mean(self.data.Y[Z==0])
         self.stats = self.cal_stats(self.data.Z)
         ate, se = self.cal_dm(self.data.Z, self.data.Y)
@@ -28,6 +33,20 @@ class Experimental(PotentialOutcome):
         
     
     def est_via_strata(self, strata):
+        """
+        Estimate with stratified data.
+
+        Parameters
+        ----------
+        strata : numpy.ndarray
+            Labels of groups.
+
+
+        Returns
+        -------
+        result class
+
+        """
         if len(strata) != self.data.n:
             raise RuntimeError("input doesn't have the same length as the data.")
         if type(strata) != np.ndarray:
@@ -45,6 +64,7 @@ class Experimental(PotentialOutcome):
 
     
     def est_via_ancova(self):
+        """Estimate with Fisher's ancova."""
         Z = self.data.Z.reshape(-1,1)
         regressor = np.concatenate((np.ones((self.data.n,1)), Z, 
                               self.data.X, self.data.X * Z), axis=1)
@@ -55,6 +75,7 @@ class Experimental(PotentialOutcome):
     
     
     def test_via_fisher(self, n=1000):
+        """Compute p-value via Fisher Randomization Test."""
         T_s = np.zeros(n)
         for i in range(n):
             Z_s = self.design.draw(self.data.n)
@@ -64,6 +85,7 @@ class Experimental(PotentialOutcome):
     
     
     def cal_dm(self, Z, Y):
+        """Helper function to calculate difference-in-mean"""
         ate = np.mean(Y[Z==1]) - np.mean(Y[Z==0])
         v1 = np.var(Y[Z==1])
         v2 = np.var(Y[Z==0])
