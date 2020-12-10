61dc85083db5818fdcd3bf5fcd3cc9cc92cb4b45
freshtaste <liujizhou123@hotmail.com>
2020-12-09 20:22:04 -0800

    add notations

--- causalmodel/designs/base.py
+++ causalmodel/designs/base.py
@@ -3,10 +3,15 @@ from causalmodel.potentialoutcome import POdata
 
 
 class DesignBase(object):
-    
+    """Base Class for design classes."""
     
     def __init__(self, params, covariate=None, balance=False,
                  eps=0.1, max_iter=1000):
+        """
+        Initialization of design classes requires a parameter. The rest of the
+        optional parameters are meant for covariate balancing with rerandomization.
+
+        """
         self.params = params
         self.X = covariate
         self.balance = balance
@@ -15,16 +20,38 @@ class DesignBase(object):
     
     
     def get_params_via_obs(self, Z):
+        """Estimate the design parameter from one realization"""
         pass
     
     
     def draw(self, n):
+        """Randomly draw treatment allocations"""
         pass
     
     
 def get_balance(Z, X):
+    """
+    Helper function to calculate balancing criterion. 
+    See: "Kari Lock Morgan & Donald B. Rubin (2015) Rerandomization to Balance 
+    Tiers of Covariates, Journal of the American Statistical Association, 
+    110:512, 1412-1421, DOI: 10.1080/01621459.2015.1079528"
+
+    Parameters
+    ----------
+    Z : TYPE
+        DESCRIPTION.
+    X : TYPE
+        DESCRIPTION.
+
+    Returns
+    -------
+    m : TYPE
+        DESCRIPTION.
+
+    """
     n = len(Z)
-    data = POdata(np.zeros(n), Z, X)
+    pseudoY = np.zeros(n)
+    data = POdata(pseudoY, Z, X)
     m1 = np.mean(data.Xt,axis=0)
     m0 = np.mean(data.Xc,axis=0)
     cov = np.cov(X.T)
