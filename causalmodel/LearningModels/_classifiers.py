1e21cf71c66a5e804928424d46ea8505c24fdcce
freshtaste <liujizhou123@hotmail.com>
2021-01-24 21:35:40 -0800

    add random forest

--- causalmodel/LearningModels/_classifiers.py
+++ causalmodel/LearningModels/_classifiers.py
@@ -1,4 +1,5 @@
 from sklearn.linear_model import LogisticRegression as Logit
+from sklearn.ensemble import RandomForestClassifier
 import numpy as np
 
 
@@ -45,3 +46,30 @@ class MultiLogisticRegression(Logit):
     def insample_proba(self):
         sklearn_out = self.predict_proba(self.X)
         return sklearn_out[np.arange(len(sklearn_out)), self.y]
+    
+
+class RandomForest(RandomForestClassifier):
+    
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        self.X = None
+        self.y = None
+        
+    
+    def fit(self, X, y):
+        if set(y) == {0, 1}:
+            self.X = X
+            self.y = y
+            return super(RandomForestClassifier, self).fit(X, y)
+        else:
+            raise RuntimeError("Input independent variable (y) is not binary!") 
+            
+    
+    def predict_proba(self, X):
+        sklearn_out = super(RandomForestClassifier, self).predict_proba(X)
+        return sklearn_out[:,1]
+    
+    
+    def insample_proba(self):
+        return self.predict_proba(self.X) 
+    
--- causalmodel/LearningModels/_predictors.py
+++ causalmodel/LearningModels/_predictors.py
@@ -1,4 +1,5 @@
 from sklearn.linear_model import LinearRegression
+from sklearn.ensemble import RandomForestRegressor
 
 
 class OLS(LinearRegression):
@@ -17,6 +18,28 @@ class OLS(LinearRegression):
     
     def predict(self, X):
         return super(OLS, self).predict(X)
+    
+    
+    def insample_predict(self):
+        return self.predict(self.X)
+    
+    
+class RandomForest(RandomForestRegressor):
+    
+    def __init__(self, **kwargs):
+        self.X = None
+        self.y = None
+        super().__init__(**kwargs)
+        
+    
+    def fit(self, X, y):
+        self.X = X
+        self.y = y
+        return super(RandomForest, self).fit(X, y)
+    
+    
+    def predict(self, X):
+        return super(RandomForest, self).predict(X)
     
     
     def insample_predict(self):
